55478fd3d79f885d65a5da993986ad7cc9087b7c
Sean Huber <sean.huber@cox.net>
2008-06-05 15:31:22 -0700

    Added more request tests

--- lib/huberry/subdomain_account/request_methods.rb
+++ lib/huberry/subdomain_account/request_methods.rb
@@ -2,7 +2,19 @@ module Huberry
 	module SubdomainAccount
 		module RequestMethods
 			def domain_with_port(tld_length = 1)
-				@domain_with_port ||= domain(tld_length) + port_string
+				domain(tld_length) + port_string
+			end
+			
+			def host_with_subdomain(sub = subdomain)
+				parts = host.split('.')
+				parts[0] = sub if parts.size > 2
+				parts.join('.')
+			end
+			
+			def host_without_subdomain
+				parts = host.split('.')
+				parts.delete_at(0) if parts.size > 2
+				parts.join('.')
 			end
 			
 			def subdomain
--- test/_request_test.rb
+++ test/_request_test.rb
@@ -33,15 +33,22 @@ class RequestTest < Test::Unit::TestCase
 	end
 	
 	def test_domain_with_port_with_non_default_port
-		@request.host = 'test.localhost.com'
 		@request.port = '8080'
 		assert_equal 'localhost.com:8080', @controller.request.domain_with_port
 	end
 	
 	def test_domain_with_port_with_default_port
-		@request.host = 'test.localhost.com'
 		@request.port = '80'
 		assert_equal 'localhost.com', @controller.request.domain_with_port
+	end
+	
+	def test_host_with_subdomain
+		assert_equal 'test.localhost.com', @controller.request.host_with_subdomain
+		assert_equal 'example.localhost.com', @controller.request.host_with_subdomain('example')
+	end
+	
+	def test_host_without_subdomain
+		assert_equal 'localhost.com', @controller.request.host_without_subdomain
 	end
 	
 	def test_subdomain_with_subdomain
