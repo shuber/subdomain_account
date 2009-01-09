7cf865b41e17fa60111e77f3a429bdd8992ab23d
Sean Huber <shuber@huberry.com>
2009-01-09 10:23:27 -0800

    Convert to soft tabs

--- lib/huberry/subdomain_account/controller_methods.rb
+++ lib/huberry/subdomain_account/controller_methods.rb
@@ -1,54 +1,54 @@
 module Huberry
-	module SubdomainAccount
-		module ControllerMethods			
-			def self.extended(base)
-				base.class_eval do
-					include InstanceMethods
-					include AuthenticationPatch if included_modules.collect(&:to_s).include? 'Huberry::Authentication::ControllerMethods::InstanceMethods'
-
-					cattr_accessor :subdomain_account_model, :subdomain_field
-					self.subdomain_account_model = 'Account'
-					self.subdomain_field = :subdomain
-					
-					attr_accessor :current_account
-					helper_method :current_account
-				end
-			end
-			
-			module InstanceMethods
-				protected
+  module SubdomainAccount
+    module ControllerMethods
+      def self.extended(base)
+        base.class_eval do
+          include InstanceMethods
+          include AuthenticationPatch if included_modules.collect(&:to_s).include? 'Huberry::Authentication::ControllerMethods::InstanceMethods'
+          
+          cattr_accessor :subdomain_account_model, :subdomain_field
+          self.subdomain_account_model = 'Account'
+          self.subdomain_field = :subdomain
+          
+          attr_accessor :current_account
+          helper_method :current_account
+        end
+      end
+      
+      module InstanceMethods
+        protected
           def find_current_account(force_query = false)
-						if @queried_for_current_account.nil? || force_query
-							account_class = self.class.subdomain_account_model.to_s.constantize
-							self.current_account = account_class.send "find_by_#{self.class.subdomain_field}", request.subdomain
-						end
-						self.current_account
+            if @queried_for_current_account.nil? || force_query
+              account_class = self.class.subdomain_account_model.to_s.constantize
+              self.current_account = account_class.send "find_by_#{self.class.subdomain_field}", request.subdomain
+            end
+            self.current_account
           end
-					
-					def subdomain_account_not_found
-						respond_to do |format|
-							format.html { render :file => "#{RAILS_ROOT}/public/404.html", :status => 404 }
-							format.all { render :nothing => true, :status => 404 }
-						end
-						false
-					end
-
-					def subdomain_account_required
-						subdomain_account_not_found if find_current_account.nil?
-					end
-			end
-			
-			module AuthenticationPatch
-				protected
-					def find_current_user(force_query = false)
-						if @queried_for_current_user.nil? || force_query
-							@queried_for_current_user = true
-							users = self.class.authentication_model.to_s.underscore.pluralize
-          		self.current_user = find_current_account.send(users).find(session[:user_id]) rescue nil
-						end
-						self.current_user
-	        end
-			end
-		end
-	end
+          
+          def subdomain_account_not_found
+            respond_to do |format|
+              format.html { render :file => "#{RAILS_ROOT}/public/404.html", :status => 404 }
+              format.all { render :nothing => true, :status => 404 }
+            end
+            false
+          end
+          
+          def subdomain_account_required
+            subdomain_account_not_found if find_current_account.nil?
+          end
+      end
+      
+      module AuthenticationPatch
+        protected
+          def find_current_user(force_query = false)
+            if @queried_for_current_user.nil? || force_query
+              @queried_for_current_user = true
+              users = self.class.authentication_model.to_s.underscore.pluralize
+              self.current_user = find_current_account.send(users).find(session[:user_id]) rescue nil
+            end
+            self.current_user
+          end
+      end
+    end
+  end
 end
--- lib/huberry/subdomain_account/request_methods.rb
+++ lib/huberry/subdomain_account/request_methods.rb
@@ -1,24 +1,24 @@
 module Huberry
-	module SubdomainAccount
-		module RequestMethods
-			def domain_with_port(tld_length = 1)
-				domain(tld_length) + port_string
-			end
-			
-			def host_with_subdomain(sub = subdomain, tld_length = 1)
-				result = sub.nil? ? '' : "#{sub}."
-				result << domain_with_port(tld_length)
-			end
-			
-			def host_without_subdomain
-				parts = host.split('.')
-				parts.delete_at(0) if parts.size > 2
-				parts.join('.')
-			end
-			
-			def subdomain
-				subdomains.first
-			end
-		end
-	end
+  module SubdomainAccount
+    module RequestMethods
+      def domain_with_port(tld_length = 1)
+        domain(tld_length) + port_string
+      end
+      
+      def host_with_subdomain(sub = subdomain, tld_length = 1)
+        result = sub.nil? ? '' : "#{sub}."
+        result << domain_with_port(tld_length)
+      end
+      
+      def host_without_subdomain
+        parts = host.split('.')
+        parts.delete_at(0) if parts.size > 2
+        parts.join('.')
+      end
+      
+      def subdomain
+        subdomains.first
+      end
+    end
+  end
 end
